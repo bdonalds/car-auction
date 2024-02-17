b513cbbbcc404104fb98a89d1d31949630da8d1c
bdonalds <bdonalds@gmail.com>
2024-02-17 15:33:57 -0500

    Next js time (#5)
    * react with usestate
    
    * end of section 8

---
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -0,0 +1,34 @@
+'use client'
+import React from 'react'
+import Countdown from 'react-countdown';
+import { zeroPad } from 'react-countdown'; 
+
+type Props = {
+    auctionEnd: string;
+}
+
+const renderer = ({days, hours, minutes, seconds, completed}: {days:number, hours:number, minutes:number, seconds:number, completed:boolean}) => {
+
+    return (
+        <div className={`
+            border-2 border-white text-white py-1 px-2 rounder-lg flex justify-center
+            ${completed ? 'bg-red-600' : (days === 0 && hours <10 ? 'bg-amber-600': 'bg-green-600')}
+        `}>
+            {completed ? (
+                <span>Auction finished</span>
+            ): (
+                <span suppressHydrationWarning={true}>
+                    {zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+                </span>
+            )}
+        </div>
+    )
+}
+
+export const CountdownTimer = ({auctionEnd}: Props) => {
+  return (
+    <div>
+        <Countdown date={auctionEnd} renderer={renderer} />
+    </div>
+  )
+}
