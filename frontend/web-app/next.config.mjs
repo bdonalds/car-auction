92b2e729f1cc10b8a74be5a20f36ea9417dc391f
Bryan Donaldson <bdonalds@gmail.com>
2024-02-17 12:22:17 -0500

    react with usestate

---
+++ frontend/web-app/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images:{
+    domains: [
+      'cdn.pixabay.com'
+    ]
+  },
+  logging: {
+      fetches: {
+        fullUrl: true,
+      },
+    },
+};
+
+export default nextConfig;
